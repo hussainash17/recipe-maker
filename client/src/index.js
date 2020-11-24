b1f5825cbff3559bfb43ba918af0fa3bc63044bf
Md Ashraf Hussain <ashrafhussain.2317.csedu@gmail.com>
2020-11-24 19:56:05 +0600

    almost done

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './components/App';
+import Navbar from './components/Navbar';
+import withSession from './components/withSession';
 import Signin from './components/Auth/Signin';
 import Signup from './components/Auth/Signup';
+import Search from './components/Recipe/Search';
+import AddRecipe from './components/Recipe/AddRecipe';
+import RecipePage from './components/Recipe/RecipePage';
+import Profile from './components/Profile/Profile';
 
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter as Router,
   Route,
@@ -17,25 +24,53 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'http://localhost:4444/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  request: (operation) => {
+    const token = localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        authorization: token,
+      },
+    });
+  },
+  onError: ({ networkError }) => {
+    if (networkError) {
+      localStorage.setItem("token", "");
+    }
+  },
 });
 
-const Root = () => {
+const Root = ({ refetch, session }) => {
   return (
     <Router>
-      <Switch>
-        <Route path='/' exact component={App}></Route>
-        <Route path='/signin' component={Signin}></Route>
-        <Route path='/signup' component={Signup}></Route>
-        <Redirect to='/'></Redirect>
-      </Switch>
+      <Fragment>
+        <Navbar session={session} />
+        <Switch>
+          <Route path='/' exact component={App} />
+          <Route path='/search' component={Search} />
+          <Route path='/signin' render={() => <Signin refetch={refetch} />} />
+          <Route path='/signup' render={() => <Signup refetch={refetch} />} />
+          <Route
+            path='/recipe/add'
+            render={() => <AddRecipe session={session} />}
+          />
+          <Route path='/recipes/:_id' component={RecipePage} />
+          <Route path='/profile' render={() => <Profile session={session} />} />
+          <Redirect to='/' />
+        </Switch>
+      </Fragment>
     </Router>
   );
 };
 
+const RootWithSession = withSession(Root);
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Root></Root>
+    <RootWithSession />
   </ApolloProvider>,
   document.getElementById('root')
 );
@@ -43,4 +78,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- server.js
+++ server.js
@@ -1,54 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
 
-const bodyParser = require('body-parser');
-const cors = require('cors')
-
-const app = express();
-
-require('dotenv').config({
-  path: 'variables.env',
-});
-
+// Bring in GraphQL-Express middleware
 const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 
-const { resolvers } = require('./resolvers');
 const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
 
-const schema = makeExecutableSchema({
-  typeDefs: typeDefs,
-  resolvers: resolvers,
-});
-
-const corsOption = {
-  origin: "http://localhost:3000",
-  credentials: true
-}
-
-app.use(cors(corsOption))
-
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: '/graphql',
-  })
-);
-
-app.use(
-  '/graphql',
-  bodyParser.json(),
-  graphqlExpress({
-    schema,
-    context: {
-      Recipe,
-      User,
-    },
-  })
-);
+const app = express();
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -63,9 +30,61 @@ mongoose
     console.log(err);
   });
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+const schema = makeExecutableSchema({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
+
+// const corsOption = {
+//   origin: 'http://localhost:3000',
+//   credentials: true,
+// };
+
+// app.use(cors(corsOption));
+
+app.use(cors('*'));
+
+// Set up JWT authentication middleware
+app.use(async (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (token !== 'null') {
+    try {
+      const currentUser = await jwt.verify(token, process.env.SECRET);
+      req.currentUser = currentUser;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  next();
+});
+
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql',
+  })
+);
+
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress(({ currentUser }) => ({
+    schema,
+    context: {
+      Recipe,
+      User,
+      currentUser,
+    },
+  }))
+);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
